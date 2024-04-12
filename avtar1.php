c01b4846b2c3fcb7ccc877f2588f18c64cafbd5c
jerry71297 <166780570+jerry71297@users.noreply.github.com>
2024-04-11 21:43:07 -0600

    Update avtar1.php

--- avtar1.php
+++ avtar1.php
@@ -1,11 +1,11 @@
 <?php
-   $respuestas = array(
-       "aire para ayudar al avatar",
-       "fuego parar ayudar a capturar al avatar",
-       "piedra para ayudar al avatar",
-       "es el avatar, y ha venido para cambiar el mundo",
-       "agua para ayudar al avatar"
-       );
-   $random = array_rand($respuestas);
-   echo $respuestas[$random];
+$respuestas = array(
+    "aire para ayudar al avatar",
+    "fuego para ayudar a capturar al avatar",
+    "piedra para ayudar al avatar",
+    "es el avatar, y ha venido para cambiar el mundo",
+    "agua para ayudar al avatar"
+);
+$random = array_rand($respuestas);
+echo $respuestas[$random];
 ?>
